eaa16f7489e69b0bda5769d5e9e7228aac2f7acd
Chan Ming Han <92656699+minghancmh@users.noreply.github.com>
2023-07-25 13:00:53 +0800

    chore: fix install_and_compile.sh

--- install_and_compile.sh
+++ install_and_compile.sh
@@ -17,20 +17,20 @@ path_to_ws=$(pwd)
 
 # #INSTALL CGAL v4.14.2
 # ##########################################
-# sudo apt-get install libgmp3-dev libmpfr-dev -y
-# mkdir -p ~/installations/cgal
-# cd ~/installations/cgal
-# wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14.2/CGAL-4.14.2.tar.xz --no-check-certificate
-# tar -xf CGAL-4.14.2.tar.xz
-# cd CGAL-4.14.2/
-# cmake . -DCMAKE_BUILD_TYPE=Release
-# sudo make install
+sudo apt-get install libgmp3-dev libmpfr-dev -y
+mkdir -p ~/installations/cgal
+cd ~/installations/cgal
+wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14.2/CGAL-4.14.2.tar.xz --no-check-certificate
+tar -xf CGAL-4.14.2.tar.xz
+cd CGAL-4.14.2/
+cmake . -DCMAKE_BUILD_TYPE=Release
+sudo make install
 
 # #INSTALL python-catkin-tools (to be able to use catkin build)
 # ##########################################
-# sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
-# wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-# sudo apt-get install python-catkin-tools -y
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get install python-catkin-tools -y
 
 #CLONE SUBMODULES, INSTALL DEPENDENCIES AND COMPILE
 ##########################################
